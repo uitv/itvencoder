d677db6c9f4f9cf430095dcd9f43203116691d10
Zhang Ping <zhangping@itv.cn>
2013-04-01 13:07:12 +0800

    class

--- test/keyvalue.c
+++ test/keyvalue.c
@@ -1,6 +1,116 @@
 #include <unistd.h>
 #include <string.h>
-#include <glib.h>
+#include <gst/gst.h>
+
+#include "keyvalue.h"
+
+GST_DEBUG_CATEGORY_EXTERN (ITVENCODER);
+#define GST_CAT_DEFAULT ITVENCODER
+
+enum {
+        CONFIGURE_PROP_0,
+        CONFIGURE_PROP_FILE_PATH,
+};
+
+static void configure_set_property (GObject *obj, guint prop_id, const GValue *value, GParamSpec *pspec);
+static void configure_get_property (GObject *obj, guint prop_id, GValue *value, GParamSpec *pspec);
+static gint configure_extract_lines (Configure *configure);
+static gint configure_replace_newline (Configure *configure);
+
+static void
+configure_class_init (ConfigureClass *configureclass)
+{
+        GObjectClass *g_object_class = G_OBJECT_CLASS (configureclass);
+        GParamSpec *param;
+
+        g_object_class->set_property = configure_set_property;
+        g_object_class->get_property = configure_get_property;
+
+        param = g_param_spec_string (
+                "configure_path",
+                "configuref",
+                "configure file path",
+                "itvencoder.conf",
+                G_PARAM_WRITABLE | G_PARAM_READABLE
+        );
+        g_object_class_install_property (g_object_class, CONFIGURE_PROP_FILE_PATH, param);
+}
+
+static void
+configure_init (Configure *configure)
+{
+        configure->lines = g_array_new (FALSE, FALSE, sizeof (gchar *));
+        configure->variables = g_array_new (FALSE, FALSE, sizeof (gpointer));
+        configure->data = gst_structure_empty_new ("configure");
+        configure->key_value_data = g_key_file_new ();
+}
+
+static void
+configure_set_property (GObject *obj, guint prop_id, const GValue *value, GParamSpec *pspec)
+{
+        g_return_if_fail (IS_CONFIGURE (obj));
+
+        switch (prop_id) {
+        case CONFIGURE_PROP_FILE_PATH:
+                CONFIGURE(obj)->configure_file_path = (gchar *)g_value_dup_string (value); //TODO: should release dup string config_path?
+                break;
+        default:
+                G_OBJECT_WARN_INVALID_PROPERTY_ID(obj, prop_id, pspec);
+                break;
+        }
+}
+
+static void
+configure_get_property (GObject *obj, guint prop_id, GValue *value, GParamSpec *pspec)
+{
+        Configure *configure = CONFIGURE (obj);
+
+        switch (prop_id) {
+        case CONFIGURE_PROP_FILE_PATH:
+                g_value_set_string(value, configure->configure_file_path);
+                break;
+        default:
+                G_OBJECT_WARN_INVALID_PROPERTY_ID (obj, prop_id, pspec);
+                break;
+        }
+}
+
+GType
+configure_get_type (void)
+{
+        static GType type = 0;
+
+        if (type) return type;
+        static const GTypeInfo info = {
+                sizeof (ConfigureClass),
+                NULL, // base class initializer
+                NULL, // base class finalizer
+                (GClassInitFunc)configure_class_init,
+                NULL,
+                NULL,
+                sizeof(Configure),
+                0,
+                (GInstanceInitFunc)configure_init,
+                NULL
+        };
+        type = g_type_register_static (G_TYPE_OBJECT, "Configure", &info, 0);
+
+        return type;
+}
+
+gint
+configure_load_from_file (Configure *configure)
+{
+        GError *e = NULL;
+
+        g_file_get_contents ("configure.conf", &configure->raw, &configure->size, &e);
+        configure_extract_lines (configure);
+
+        configure_replace_newline (configure);
+        configure_file_parse (configure);
+}
+
+GstStructure *configure_data;
 
 gint
 configure_channel_parse (gchar *name, gchar *element)
@@ -80,72 +190,69 @@ configure_channel_parse (gchar *name, gchar *element)
 }
 
 gint
-configure_file_parse (gchar *conf, gsize size)
+configure_file_parse (Configure *configure)
 {
-        GKeyFile *configure;
         GKeyFileFlags flags;
         GError *e = NULL;
         gsize number;
         gchar **p, *v;
         gint i;
+        GstStructure *structure;
+        GValue value = { 0, { { 0 } } };
 
-        configure = g_key_file_new ();
+        g_value_init (&value, G_TYPE_STRING);
+
         flags = G_KEY_FILE_KEEP_COMMENTS | G_KEY_FILE_KEEP_TRANSLATIONS;
-        if (!g_key_file_load_from_data (configure, conf, size, flags, &e)) {
+        if (!g_key_file_load_from_data (configure->key_value_data, configure->ini_raw, strlen (configure->ini_raw), flags, &e)) {
                 g_error (e->message);
                 return 1;
         }
 
-        p = g_key_file_get_keys (configure, "server", &number, &e);
+        p = g_key_file_get_keys (configure->key_value_data, "server", &number, &e);
         if (e != NULL) {
                 g_error (e->message);
                 return 1;
         }
-        g_print ("number is %d\n", number);
+        structure = gst_structure_empty_new ("server");
         for (i = 0; i < number; i++) {
-                v = g_key_file_get_value (configure, "server", p[i], &e);
-                g_print ("%s - %s\n", p[i], v);
+                v = g_key_file_get_value (configure->key_value_data, "server", p[i], &e);
+                g_value_set_static_string (&value, v);
+                gst_structure_set_value (structure, p[i], &value);
         }
         g_strfreev (p);
+        gst_structure_set (configure->data, "server", GST_TYPE_STRUCTURE, structure, NULL);
 
-        p = g_key_file_get_keys (configure, "channel", &number, &e);
-        g_print ("number is %d\n", number);
+        p = g_key_file_get_keys (configure->key_value_data, "channel", &number, &e);
+        structure = gst_structure_empty_new ("channel");
         for (i = 0; i < number; i++) {
-                v = g_key_file_get_value (configure, "channel", p[i], &e);
-                g_print ("%s - %s\n", p[i], v);
+                v = g_key_file_get_value (configure->key_value_data, "channel", p[i], &e);
                 configure_channel_parse (p[i], v);
+                g_value_set_static_string (&value, v);
+                gst_structure_set_value (structure, p[i], &value);
         }
         g_strfreev (p);
+        gst_structure_set (configure->data, "channel", GST_TYPE_STRUCTURE, structure, NULL);
+
+        return 0;
 }
 
-typedef struct _ConfigurableVar {
-        gint index;
-        gchar *name;
-        gchar *type;
-        gchar *description;
-        gchar *value;
-} ConfigurableVar;
-
-gchar *
-configure_extract_variable (gchar *conf, gsize size)
+static gint
+configure_extract_lines (Configure *configure)
 {
         gchar *p, *p1, *p2, *p3, *p4, *p5; 
         GRegex *reg;
         GError *e = NULL;
-        GArray *conf_array;
-        GArray *variable_array;
-        ConfigurableVar *configurable_var;
+        ConfigurableVar *variable;
         gint i, line_number, index;
         gchar var_status;
+        GstStructure *configure_mgmt;
 
-        conf_array = g_array_new (FALSE, FALSE, sizeof (gchar *));
-        variable_array = g_array_new (FALSE, FALSE, sizeof (gpointer));
-        p = g_strdup_printf ("%s", conf);
+        p = g_strdup_printf ("%s", configure->raw);
         p1 = p2 = p3 = p;
         line_number = 0;
         index = 0;
         for (;;) {
-                if (p1 - p >= size) break;
+                if (p1 - p >= configure->size) break;
 
                 /* process one line every time */
                 while ((*p2 != '\n') && (*p2 != '\0')) {
@@ -161,12 +268,12 @@ configure_extract_variable (gchar *conf, gsize size)
                         case '\n': /* end of line. */
                                 if ((var_status == '\0') || (var_status == 'v')) {
                                         p5 = g_strndup (p4, p2 - p4 + 1);
-                                        g_array_append_val (conf_array, p5);
+                                        g_array_append_val (configure->lines, p5);
                                         index++;
                                         p3 = p2;
                                 } else {
                                         g_print ("line %d position %d: %s, parse error\n", line_number, p3 - p1, p1);
-                                        return NULL;
+                                        return 1;
                                 }
                                 break;
                         case '<':
@@ -177,16 +284,16 @@ configure_extract_variable (gchar *conf, gsize size)
                                         /* variable value */
                                         var_status = '/';
                                         p5 = g_strndup (p4, p3 - p4);
-                                        g_array_append_val (conf_array, p5);
-                                        configurable_var = g_malloc (sizeof (ConfigurableVar));
-                                        configurable_var->value = g_strdup (p5);
-                                        configurable_var->index = index;
-                                        g_array_append_val (variable_array, configurable_var);
+                                        g_array_append_val (configure->lines, p5);
+                                        variable = g_malloc (sizeof (ConfigurableVar));
+                                        variable->value = g_strdup (p5);
+                                        variable->index = index;
+                                        g_array_append_val (configure->variables, variable);
                                         index++;
                                         p4 = p3;
                                 } else {
                                         g_print ("line %d, position %d: %s, parse error\n", line_number, p3 - p1, p1);
-                                        return NULL;
+                                        return 1;
                                 }
                                 break;
                         case '>':
@@ -194,7 +301,7 @@ configure_extract_variable (gchar *conf, gsize size)
                                         /* open variable define */
                                         var_status = '>';
                                         p5 = g_strndup (p4, p3 - p4 + 1);
-                                        g_array_append_val (conf_array, p5);
+                                        g_array_append_val (configure->lines, p5);
                                         index++;
                                         p4 = p3 + 1;
                                 } else if (var_status == '/') {
@@ -202,7 +309,7 @@ configure_extract_variable (gchar *conf, gsize size)
                                         var_status = 'v';
                                 } else {
                                         g_print ("line %d position %d: %s, parse error\n", line_number, p3 - p1, p1);
-                                        return NULL;
+                                        return 1;
                                 }
                                 break;
                         }
@@ -216,28 +323,29 @@ configure_extract_variable (gchar *conf, gsize size)
                 p2++;
                 p1 = p3 = p2;
         }
+
         g_free (p);
-        for (i = 0; i < conf_array->len; i++) {
-                //g_print ("%s\n", g_array_index (conf_array, gchar *, i));
+        for (i = 0; i < configure->lines->len; i++) {
+                g_print ("%s\n", g_array_index (configure->lines, gchar *, i));
         }
-        for (i = 0; i < variable_array->len; i++) {
-                configurable_var = g_array_index (variable_array, gpointer, i);
-                g_print ("index %d value %s\n", configurable_var->index, configurable_var->value);
+        for (i = 0; i < configure->variables->len; i++) {
+                variable = g_array_index (configure->variables, gpointer, i);
+                g_print ("index %d value %s\n", variable->index, variable->value);
         }
-        return NULL;
+        return 0;
 }
 
 /*
  * replace newline with \n, so it can be parsed by glib ini style parser.
  */
-gchar *
-configure_replace_newline (gchar *conf, gsize size)
+static gint
+configure_replace_newline (Configure *configure)
 {
         gchar *p1, *p2, *p3;
         gint right_square_bracket;
 
-        p3 = p1 = g_malloc (size * 2);
-        p2 = conf;
+        p3 = p1 = g_malloc (configure->size * 2);
+        p2 = configure->raw;
         right_square_bracket = 0;
         for (;;) {
                 switch (*p2) {
@@ -251,7 +359,8 @@ configure_replace_newline (gchar *conf, gsize size)
                         break;
                 case '\n':
                         if (right_square_bracket > 0) {
-                                *p3 = '\\'; p3++;
+                                *p3 = '\\';
+                                p3++;
                                 *p3 = 'n'; 
                         } else {
                                 *p3 = *p2;
@@ -261,29 +370,29 @@ configure_replace_newline (gchar *conf, gsize size)
                         *p3 = *p2;
                         break;
                 }
-                p2++; p3++;
-                if ((p2 - conf) == size) break;
+                p2++;
+                p3++;
+                if ((p2 - configure->raw) == configure->size) {
+                        break;
+                }
         }
         *p3 = '\0';
         p3 = g_strdup_printf ("%s", p1);
         g_free (p1);
-        return p3;
+        configure->ini_raw = p3;
+        g_print ("%s\n", configure->ini_raw);
+        return 0; 
 }
 
 gint
 main (gint argc, gchar *argv[])
 {
-        GError *e;
-        gsize size;
-        gchar *p, *conf;
-        gint right_square_bracket;
+        Configure *configure;
 
-        g_file_get_contents ("configure.conf", &conf, &size, &e);
+        gst_init (&argc, &argv);
 
-        configure_extract_variable (conf, size);
-        
-        p = configure_replace_newline (conf, size);
-        if (configure_file_parse (p, strlen (p)) != 0) {
-                return 1;
-        }
+        configure = configure_new ("configure_path", "configure.conf", NULL);
+        configure_load_from_file (configure);
+
+        g_print ("\n\n\nHello, gstructure %s!\n\n\n", gst_structure_to_string (configure->data));
 }
---
+++ test/keyvalue.h
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (c) 2012 iTV.cn
+ *  Author Zhang Ping <zhangping@itv.cn>
+ */
+
+#ifndef __CONFIGURE_H__
+#define __CONFIGURE_H__
+
+#include <gst/gst.h>
+
+typedef struct _Configure          Configure;
+typedef struct _ConfigureClass     ConfigureClass;
+
+typedef struct _ConfigurableVar {
+        gint index;
+        gchar *name;
+        gchar *type;
+        gchar *description;
+        gchar *value;
+} ConfigurableVar;
+
+struct _Configure {
+        GObject parent;
+
+        gchar *configure_file_path;
+        
+        /* raw configure file content */
+        gchar *raw;
+
+        /* raw configure file content size */
+        gsize size;
+
+        /* replace return in raw with \n, for ini parse */
+        gchar *ini_raw;
+
+        /* glib key value type data. */
+        GKeyFile *key_value_data;
+
+        /* GstStructure type of configure data */
+        GstStructure *data;
+
+        /* Line array of configure, very configurable var should take one line */
+        GArray *lines;
+
+        /* Configuralbe var array */
+        GArray *variables;
+};
+
+struct _ConfigureClass {
+        GObjectClass parent;
+};
+
+#define TYPE_CONFIGURE           (configure_get_type())
+#define CONFIGURE(obj)           (G_TYPE_CHECK_INSTANCE_CAST ((obj), TYPE_CONFIGURE, Configure))
+#define CONFIGURE_CLASS(cls)     (G_TYPE_CHECK_CLASS_CAST    ((cls), TYPE_CONFIGURE, ConfigureClass))
+#define IS_CONFIGURE(obj)        (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TYPE_CONFIGURE))
+#define IS_CONFIGURE_CLASS(cls)  (G_TYPE_CHECK_CLASS_TYPE    ((cls), TYPE_CONFIGURE))
+#define CONFIGURE_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS  ((obj), TYPE_CONFIGURE, ConfigureClass))
+#define configure_new(...)       (g_object_new(TYPE_CONFIGURE, ## __VA_ARGS__, NULL))
+
+GType configure_get_type (void);
+gint configure_load_from_file (Configure *configure);
+gint configure_save_to_file (Configure *configure);
+
+#endif /* __CONFIGURE_H__ */
