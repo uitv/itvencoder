9da5c3060f7f4c47be143409ebd80aab87e80032
Zhang Ping <zhangping@itv.cn>
2013-09-11 08:58:37 +0800

    test

--- test/httpmgmt.py
+++ test/httpmgmt.py
@@ -35,7 +35,6 @@ while True:
     request("192.168.2.10", 20118, 8, "start")
     request("192.168.2.10", 20118, 9, "start")
     """
-    time.sleep(27)
     request("192.168.2.10", 20118, 0, "restart")
     request("192.168.2.10", 20118, 1, "restart")
     time.sleep(1)
